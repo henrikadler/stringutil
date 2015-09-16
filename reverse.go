543837641621c5142854e220376fbf70e5fb1614
Henrik Adler <henrik.adler@tv4.se>
2015-09-16 23:43:19 +0200

    fix unit test
    Who approved this :) I did for fun..

--- reverse.go
+++ reverse.go
@@ -1,8 +1,6 @@
 // Package stringutil contains utility functions for working with strings.
 package stringutil
 
-import "fmt"
-
 // Reverse returns its argument string reversed rune-wise left to right.
 func Reverse(s string) string {
 
@@ -10,8 +8,6 @@ func Reverse(s string) string {
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
-
-	fmt.Println("Hello")
 	return string(r)
 }
 
