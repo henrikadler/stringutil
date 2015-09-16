c9e0f5e96a33baaa2a93b76ac379e6e785c47a7b
Henrik Adler <henrik.adler@tv4.se>
2015-09-16 20:27:10 +0200

    Initial commit

---
+++ reverse_test.go
@@ -0,0 +1,22 @@
+package stringutil
+
+import "testing"
+
+//Test the stringutil.Reverse function...
+func TestReverse(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
+
+	//Now simply loop thru all cases and error out on failures...
+	for _, c := range cases {
+		got := Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
